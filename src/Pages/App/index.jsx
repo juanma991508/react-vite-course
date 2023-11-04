f13a59ebba03abe81d27ad8c339d4663685f7f3a
Juan Manuel <juanma1456.jg@gmail.com>
2023-11-04 19:20:17 +0100

    add context

--- src/Pages/App/index.jsx
+++ src/Pages/App/index.jsx
@@ -1,4 +1,5 @@
 import { useRoutes, BrowserRouter } from 'react-router-dom';
+import { ShoppingCartProvider } from '../../Context'
 import Home from '../Home'
 import MyAccount from '../MyAccount'
 import MyOrder from '../MyOrder'
@@ -27,10 +28,12 @@ const App = () =>
 {
   return (
     <>
+    <ShoppingCartProvider>
       <BrowserRouter>
         <AppRoutes/>
         <NavBar/>
       </BrowserRouter>
+    </ShoppingCartProvider>
     </>
   )
 }
